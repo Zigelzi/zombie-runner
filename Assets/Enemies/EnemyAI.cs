097c63837de9c7881d95135ee370876347c5d865
Miika Savela <miika.a.savela@gmail.com>
2021-07-11 14:52:42 +0300

    Add sandbox level, basic movement and basic navmesh movement for chasing the player for enemy

---
+++ Assets/Enemies/EnemyAI.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    NavMeshAgent navMeshAgent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        Player player = FindObjectOfType<Player>();
+        if (player != null)
+        {
+            target = player.transform;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveToTarget();
+    }
+
+    void MoveToTarget()
+    {
+        navMeshAgent.SetDestination(target.position);
+    }
+}
