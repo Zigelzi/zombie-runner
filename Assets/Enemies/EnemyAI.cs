a9d55c88b6309dc6704bb53f148c9926e2e64702
Miika Savela <miika.a.savela@gmail.com>
2021-07-11 16:31:51 +0300

    Add chasing and engaging behaviour

--- Assets/Enemies/EnemyAI.cs
+++ Assets/Enemies/EnemyAI.cs
@@ -6,7 +6,12 @@ using UnityEngine.AI;
 public class EnemyAI : MonoBehaviour
 {
     [SerializeField] Transform target;
+    [SerializeField] [Range(0, 20f)] float chaseRange = 5f;
+
     NavMeshAgent navMeshAgent;
+    float distanceToTarget = Mathf.Infinity;
+    bool isProvoked = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,11 +27,75 @@ public class EnemyAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        MoveToTarget();
+        if (isProvoked)
+        {
+            EngageTarget();
+        } 
+        else if (IsTargetInChaseRange())
+        {
+            isProvoked = true;
+            ChaseTarget();
+        }
+        
     }
 
-    void MoveToTarget()
+    void OnDrawGizmosSelected()
+    {
+        DisplayChaseRange();
+    }
+
+    void EngageTarget()
+    {
+        if (!IsTargetInAttackRange())
+        {
+            ChaseTarget();
+        }
+
+        if (IsTargetInAttackRange())
+        {
+            AttackTarget();
+        }
+    }
+
+    bool IsTargetInChaseRange()
+    {
+        distanceToTarget = Vector3.Distance(target.position, transform.position);
+        if (distanceToTarget <= chaseRange)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    void ChaseTarget()
     {
         navMeshAgent.SetDestination(target.position);
+    }
+
+    bool IsTargetInAttackRange()
+    {
+        distanceToTarget = Vector3.Distance(target.position, transform.position);
+
+        if (distanceToTarget <= navMeshAgent.stoppingDistance)
+        {
+            return true;
+        } else
+        {
+            return false;
+        }
+    }
+
+    void AttackTarget()
+    {
+        Debug.Log("Reeee!");
+    }
+
+    void DisplayChaseRange()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
     }
 }
